1d88943a468a047bffaf6f7c6bc391c5b80a16fe
mal-wood <mallorywood413@gmail.com>
2022-09-13 18:03:03 -0400

    save last feature

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,17 @@
+import useLocalStorage from "../hooks/useLocalStorage"
+
 const Search = () => {
 
+  const [searchTerm, setSearchTerm] = useLocalStorage('search', 'React')
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement> ) => {
-    console.log(event.target.value)
+    setSearchTerm(event.target.value);
   }
 
   return (
     <div>
       <label htmlFor="search"> Search: </label>
-      <input id="search" type="search" onChange={handleChange}></input>
+      <input id="search" type="text" value={searchTerm} onChange={handleChange}></input>
     </div>
   )
 }
